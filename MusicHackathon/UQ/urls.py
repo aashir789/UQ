41a2eecf1360f9e8cf7c1abd6c90304ec781a10c
emaashir789/em <aashir789@gmail.com>
2015-07-25 17:45:21 -0400

    added view for lists

--- MusicHackathon/UQ/urls.py
+++ MusicHackathon/UQ/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'UQ.views.home', name='home'),
     url(r'^playLink', 'UQ.views.playLink', name='playLink'),
+    url(r'^login$', 'django.contrib.auth.views.login', {'template_name':'UQ/login.html'}, name='login'),
+    url(r'^logout$', 'django.contrib.auth.views.logout_then_login', name='logout'),
 )
