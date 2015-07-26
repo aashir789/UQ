c6a95d9b9c825d65a18c098daf335466c7cc5b72
emaashir789/em <aashir789@gmail.com>
2015-07-25 21:31:11 -0400

    last

--- MusicHackathon/UQ/static/UQ/vote.js
+++ MusicHackathon/UQ/static/UQ/vote.js
@@ -1,9 +1,19 @@
 function upvote(songid){
     var url = "/upVote/"+songid
-    $.get(url);
+    $.get(url, function(data){
+	ele = document.getElementById('song'+songid);
+	eleVal = parseInt(ele.innerHTML)+1;
+	
+	ele.innerHTML= eleVal; 
+    });
 }
 
 function downvote(songid){
     var url = "/downVote/"+songid
-    $.get( url );
+    $.get(url, function(data){
+	ele = document.getElementById('song'+songid);
+	eleVal = parseInt(ele.innerHTML)-1;
+	
+	ele.innerHTML= eleVal; 
+    });
 }
